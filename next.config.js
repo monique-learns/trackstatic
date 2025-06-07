59f047ddeef2414739ab6e21f7c08dc290339478
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2025-06-07 14:10:39 -0500

    update next config  js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -20,4 +24,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
